f2acb0442b6ceb44598b3251b2eeef02895842c9
Adam Yang <ayang@wpi.edu>
2022-02-04 13:47:31 -0500

    created bins and program class

---
+++ src/Genetics.java
@@ -0,0 +1,19 @@
+public class Genetics {
+
+    public Genetics() {
+
+    }
+
+/*
+genetic representation:
+4:      1 0 0
+2.5:
+
+
+
+ */
+
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
